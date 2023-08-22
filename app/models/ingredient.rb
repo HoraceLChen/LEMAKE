01dc32eb7f4d4da00366300401a73021fa6c1f1d
Horace Chen <horacechen2@gmail.com>
2023-08-22 17:18:51 +1000

    merge update

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,3 @@
 class Ingredient < ApplicationRecord
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
--- db/migrate/20230821083733_create_meals.rb
+++ db/migrate/20230821083733_create_meals.rb
@@ -2,7 +2,7 @@ class CreateMeals < ActiveRecord::Migration[7.0]
   def change
     create_table :meals do |t|
       t.references :user, null: false, foreign_key: true
-      t.references :recipe, null: false, foreign_key: true
+      t.references :recipe, null: true, foreign_key: true
       t.string :cuisine_preference
       t.string :img
       t.integer :time_preference
