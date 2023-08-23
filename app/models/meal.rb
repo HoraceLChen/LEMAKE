2e4ad64a239626253bce9eb0d5fab97ea03e12ba
Vincent Tee <vincent.teeym@gmail.com>
2023-08-23 17:45:25 +1000

    Reviews Associations Fixed

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -4,4 +4,5 @@ class Meal < ApplicationRecord
   has_many :uploaded_ingredients, dependent: :destroy
   has_many :ingredients, through: :uploaded_ingredients
   has_many_attached :photos
+  has_one :review
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,7 @@
 class Recipe < ApplicationRecord
   has_many :recipe_ingredients, dependent: :destroy
   has_many :ingredients, through: :recipe_ingredients
+  has_many :meals
+  has_many :reviews, through: :meals
   acts_as_favoritable
 end
