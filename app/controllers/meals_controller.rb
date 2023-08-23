de77cdff5cba173a50cc977cce7baede72c51364
exec zsh <kaishu0520@gmail.com>
2023-08-23 11:29:41 +0900

    some controllers fix

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -5,11 +5,13 @@ class MealsController < ApplicationController
   end
 
   def new
-    @meal = Meal.new # links to camera outside app
+    @meal = Meal.new
+    # new_uploaded_ingredient
   end
 
   def create
-    @meal = Meal.new(meal_params) #=> google lends comes here?
+    @meal = Meal.new(meal_params)
+    # new_uploaded_ingredient
     @meal.user_id = cerrent_user.id
     if @meal.save
       redirect_to uploaded_ingredients_index_path
@@ -22,17 +24,21 @@ class MealsController < ApplicationController
   end
 
   def update
-    @meal.update(meal_params) # => not updating
+    @meal.update!(meal_params)
     redirect_to meal_recipes_path(@meal)
   end
 
   private
 
   def meal_params
-    params.require(:meal).permit(:user_id, :img)
+    params.require(:meal).permit(:user_id, :img, :time_preference, :people_preference, :cuisine_preference)
   end
 
   def set_meal
     @meal = Meal.find(params[:id])
   end
+
+#   def new_uploaded_ingredient
+#     @uploaded_ingredient = UploadedIngredient.new
+#   end
 end
