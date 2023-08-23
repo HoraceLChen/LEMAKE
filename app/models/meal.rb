4adf99a1888e8d65b3f122baed28483bbc6b14a1
Vincent Tee <vincent.teeym@gmail.com>
2023-08-23 17:24:26 +1000

    Create Uploaded_Ingredients and delete

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,7 +1,7 @@
 class Meal < ApplicationRecord
   belongs_to :user
   belongs_to :recipe
-  has_many :uploaded_ingredients
+  has_many :uploaded_ingredients, dependent: :destroy
   has_many :ingredients, through: :uploaded_ingredients
   has_many_attached :photos
 end
