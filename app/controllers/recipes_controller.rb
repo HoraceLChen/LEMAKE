71a0f4e60e6a9ff69265eedaf215cc115a5a3495
Horace Chen <horacechen2@gmail.com>
2023-08-24 17:12:54 +1000

    API working, Recipe instances are not created though

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,12 +4,12 @@ class RecipesController < ApplicationController
   def index
     api_key = ENV['SPOONACULAR_API_KEY']
     service = SpoonacularService.new(api_key)
-    ingredients = params[:ingredients].split(',') # Assuming you're passing ingredients as a comma-separated list
+    @meal = Meal.find(params[:meal_id])
+    ingredients = @meal.ingredients.map { |ingredient| ingredient.name } # Assuming you're passing ingredients as a comma-separated list
     @recipes = service.search_by_ingredients(ingredients)
-
+    # raise
     if @recipes
-      @recipes = Recipe.limit(3) #This comes from API
-      @meal = Meal.find(params[:meal_id])
+      @recipes
     else
       render template: 'uploaded_ingredients/index'
 
--- app/services/spoonacular_service.rb
+++ app/services/spoonacular_service.rb
@@ -1,18 +1,34 @@
 class SpoonacularService
-  BASE_URL = 'https://api.spoonacular.com/recipes/findByIngredients'
+  BASE_URL = 'https://api.spoonacular.com/recipes/complexSearch'
 
   def initialize(api_key)
     @api_key = api_key
     @connection = Faraday.new(url: BASE_URL)
+    @recipes = []
   end
 
   def search_by_ingredients(ingredients)
     response = @connection.get do |req|
       req.url BASE_URL
       req.params['apiKey'] = @api_key
-      req.params['ingredients'] = ingredients.join(',')
+      req.params['includeIngredients'] = ingredients.join(',')
+      # req.params['type'] = "main course"
+      req.params['instructionsRequired'] = true
+      req.params['fillIngredients'] = false
+      req.params['addRecipeInformation'] = true
+      req.params['number'] = 6
     end
+    results = JSON.parse(response.body) if response.status == 200
 
-    JSON.parse(response.body) if response.status == 200
+    results["results"].each do |result|
+      # p result
+      # # raise
+      # Recipe.new(
+      # title: result["title"],
+      # cuisine: result["image"],
+      # time: result["readyInMinutes"]
+      # )
+        # @recipes.push(recipe)
+    end
   end
 end
