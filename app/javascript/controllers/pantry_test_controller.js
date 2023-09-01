c07ba0b96c387895ca8aee98c05eb0edacabd725
Vincent Tee <vincent.teeym@gmail.com>
2023-09-01 15:36:21 +1000

    Removed Notes JS

--- app/javascript/controllers/pantry_test_controller.js
+++
@@ -1,8 +0,0 @@
-import { Controller } from "@hotwired/stimulus"
-
-// Connects to data-controller="pantry-test"
-export default class extends Controller {
-  connect() {
-    console.log("connected to pantry test controller");
-  }
-}
